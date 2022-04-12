2586435823ad34a0469e1ad508a0e1d308ff5747
Alex <kiratualex2@gmail.com>
2022-04-12 15:48:44 -0700

    sign

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -2,3 +2,25 @@
 
 /**
  * print_sign - prints the sign of a number
+ * @n: takes an integer
+ * Return 1 and print + if n > 0, Return 0 and print 0 if n = 0, Return -1 and print - if n < 0
+ */
+
+int print_sign(int n)
+{
+	if (n > 0)
+	{
+		_putchar('+');
+		return (1);
+	}
+	else if (n == 0)
+	{
+		_putchar('0');
+		return (0);
+	}
+	else
+	{
+		_putchar('-');
+		return (-1);
+	}
+}
