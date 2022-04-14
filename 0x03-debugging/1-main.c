5d79b3bb612d39986e50d5856cceea9166ef56dc
Alex <kiratualex2@gmail.com>
2022-04-14 06:04:54 -0700

    largest number

--- 0x03-debugging/1-main.c
+++ 0x03-debugging/1-main.c
@@ -12,7 +12,6 @@ int main(void)
 	printf("infinite loop incoming :(\n");
 
 	i = 0;
-
 	/*
 	 * while (i < 10)
 	 * {
@@ -20,5 +19,6 @@ int main(void)
 	 * }
 	 */
 	printf("infinite loop avoided! \\o/\n");
+
 	return (0);
 }
