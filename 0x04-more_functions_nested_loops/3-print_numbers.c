f54d4a0b4a49971f3a08283b54820125211cca20
Alex <kiratualex2@gmail.com>
2022-04-14 11:00:03 -0700

    numbers

--- 0x04-more_functions_nested_loops/3-print_numbers.c
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -7,10 +7,13 @@
 
 void print_numbers(void)
 {
-	char i;
+	int i;
 
-	for (i = '0'; i <= '9'; i++)
-		_putchar(i);
+	i = 0;
+
+	for (i = 0; i <= 10; i++)
+
+	_putchar(i * '0');
 
 	_putchar('\n');
 }
