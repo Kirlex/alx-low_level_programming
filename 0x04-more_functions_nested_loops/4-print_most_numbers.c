5b91830aafc0c83100e84cbc7d1b47b3ec62048b
Alex <kiratualex2@gmail.com>
2022-04-14 14:50:42 -0700

    most

--- 0x04-more_functions_nested_loops/4-print_most_numbers.c
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -7,18 +7,13 @@
 
 void print_most_numbers(void)
 {
-	int i;
+int i = '0';
 
-	for (i = 0; i < 10; i++)
-	{
+for (; i <= 9; i++)
+{
 
-		if (i == 2 || i == 4)
-
-			i++
-
-				_putchar(i + '0');
-
-	}
-	_putchar ('\n');
-
+if (i != '2'&& i != '4')
+_putchar(i);
+}
+_putchar('\n');
 }
