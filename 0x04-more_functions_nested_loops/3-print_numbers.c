3649a9e8ebe1a039b322d53733592dd37bbf6d38
Kirlex <101473736+Kirlex@users.noreply.github.com>
2022-04-18 21:22:44 +0300

    Update 3-print_numbers.c

--- 0x04-more_functions_nested_loops/3-print_numbers.c
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -1,21 +1,20 @@
-#include "main.h"                                                                             
+#include "holberton.h"
 
-/**                                                                                                   
- * print_numbers - prints numbers from 0 to 9, followed by a new line                                 
+/**
+ * print_numbers - prints 0123456789\n
  *
- * Return: Always 0. 
- */                                                                                                   
-void print_numbers(void)                                                                              
-{                                                                                                    
-	int i;
-	
-	for (i = '0'; i <= '9'; i++);
-	{                                                                                    
-		_putchar(i);                                                                          
+ * Return: void
+ */
 
+
+void print_numbers(void)
+{
+	char c;
+
+	for (c = '0'; c <= '9'; c++)
+	{
+		_putchar(c);
 	}
-	_putchar('\n');              
-
-}	
-
+	_putchar('\n');
+}
 
